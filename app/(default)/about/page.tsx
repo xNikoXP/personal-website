8b5a5c60790a407608388e672a62ef9101052172
Arvi Rivera <arvi.j.rivera@outlook.com>
2026-01-22 13:07:09 -0500

    Added more to the description of the about section

--- app/(default)/about/page.tsx
+++ app/(default)/about/page.tsx
@@ -13,8 +13,15 @@ export default function About(){
 
                 <div className='flex flex-col justify-center items-center w-[80%] text-left'>
                     <h2 className='text-[2em] font-bold'>Arvi Rivera</h2>
-                    <p className='mt-[5dvh] w-[50%] text-[1em]'>Hi, my name is Arvi. I am currently a student at Kean University. 
-                        My hobbies include, collecting audio equipment, biking, and gaming. It is a pleasure to meet you.</p>
+                    <p className='mt-[5dvh] w-[50%] text-[1em]'> &emsp; Hi, my name is Arvi. I am currently a student at Kean University. 
+                        My hobbies include, collecting audio equipment, biking, and gaming. It is a pleasure to meet you. <br/> 
+                        <br /> &emsp; Currently I am working on three projects. First, I am finishing this website. As you can see
+                        the projects tab is currently a work in progress and I am making some design changes to the blog. <br/>
+                        <br /> &emsp; Second project, I am working on is a news website called Journey's End focused on delivering geopolitical news.
+                        The project is part of a wider project of making a media brand and currrently has an instagram for it. <br />
+                        <br /> &emsp; Last project I am working on is one inspired by a youtube channel called Michael Reeves, where a device is worn 
+                        that monitors your speech and if it determines your public speaking to be inadequate it tazes you. Once the 
+                        project tab of this website is completed you will be able to see the progress there. </p>
                 </div>
 
 
